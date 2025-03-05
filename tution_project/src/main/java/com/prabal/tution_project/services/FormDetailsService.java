ed162865422ba641bb15869ea8eff297366cf495
Prerit <preritmishra436351@gmail.com>
2025-03-06 00:57:55 +0530

    minor changes

--- tution_project/src/main/java/com/prabal/tution_project/services/FormDetailsService.java
+++ tution_project/src/main/java/com/prabal/tution_project/services/FormDetailsService.java
@@ -3,15 +3,11 @@ package com.prabal.tution_project.services;
 import com.prabal.tution_project.entities.FormDetails;
 import com.prabal.tution_project.repositories.FormDetailsRepository;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
 
 @Data
 @Service
@@ -25,10 +21,7 @@ public class FormDetailsService {
     }
 
     public ResponseEntity<?> addNewUser(FormDetails formDetails) {
-        FormDetails formDetails1=repo.save(formDetails);
-        if(formDetails1!=null){
-            return ResponseEntity.ok("User created successfully!!");
-        }
-        else return ResponseEntity.badRequest().body("User couldn't be created!");
+        repo.save(formDetails);
+        return ResponseEntity.ok("User created successfully!!");
     }
 }
