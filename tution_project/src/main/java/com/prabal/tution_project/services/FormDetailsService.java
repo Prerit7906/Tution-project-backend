ee2560c061d521d23054664a8c5ed63e477a71e7
Prerit <preritmishra436351@gmail.com>
2025-03-06 11:22:55 +0530

    Implemented mail sender

--- tution_project/src/main/java/com/prabal/tution_project/services/FormDetailsService.java
+++ tution_project/src/main/java/com/prabal/tution_project/services/FormDetailsService.java
@@ -6,8 +6,9 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
 
 @Data
 @Service
@@ -16,12 +17,24 @@ public class FormDetailsService {
     @Autowired
     private FormDetailsRepository repo;
 
+    @Autowired
+    private JavaMailSender mailSender;
+
     public ResponseEntity<Iterable<FormDetails>> getAllEntries() {
         return ResponseEntity.ok(repo.findAll());
     }
 
     public ResponseEntity<?> addNewUser(FormDetails formDetails) {
         repo.save(formDetails);
+        sendEmail(formDetails.getEmail(), formDetails.getName());
         return ResponseEntity.ok("User created successfully!!");
+    }
+
+    private void sendEmail(String toEmail, String name) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject("Welcome to Our Platform");
+        message.setText("Dear " + name + ",\n\nThank you for registering with us. We are excited to have you on board!\n\nBest Regards,\nTeam VScholar");
+        mailSender.send(message);
     }
 }
